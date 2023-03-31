e63cb352814d1af01bd2a3c3e8f53ac9f41bfff8
Tianze Shou <shoutianze@outlook.com>
2023-03-31 14:50:51 -0400

    update employee routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   # Resource routes (maps HTTP verbs to controller actions automatically):
   # Routes for regular HTML views go here...
   resources :stores, except: [:destroy]
+  resources :employees
 
   # You can have the root of your site routed with 'root'
   root 'home#index'
