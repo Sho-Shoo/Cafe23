7a3e64b582664cd6975e211e8123746c7f87c450
Tianze Shou <shoutianze@outlook.com>
2023-03-31 13:50:51 -0400

    working store controller

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,54 @@
+class StoresController < ApplicationController
+    before_action :set_store, only: [:show, :edit, :update]
+    # before_action :check_login
+    # authorize_resource
+
+    def index
+        @active_stores = Store.alphabetical.active.paginate(page: params[:page]).per_page(10)
+        @inactive_stores = Store.alphabetical.inactive.paginate(page: params[:page]).per_page(10)
+    end
+
+    def show
+        current_assignments = @store.assignments.current
+        @current_employees = current_assignments.map { |a| a.employee }
+    end
+
+    def edit
+    end
+
+    def new
+        @store = Store.new
+    end
+
+    def create
+        @store = Store.new(store_params)
+        if @store.save
+            redirect_to store_path(@store), notice: "#{@store.name} was added to the system."
+        else
+            render action: 'new'
+        end
+    end
+
+    def update
+        if @store.update(store_params)
+            redirect_to store_path(@store), notice: "Updated store information for #{@store.name}."
+        else
+            render action: 'edit'
+        end
+    end
+
+    # def destroy
+    #     @store.destroy
+    #     redirect_to stores_url
+    # end
+
+    private
+    def store_params
+        params.require(:store).permit(:name, :street, :city, :state, :zip, :phone, :active)
+    end
+
+    def set_store
+        @store = Store.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,22 @@ Rails.application.routes.draw do
     end
   end
 
+  # Semi-static page routes
+  get 'home', to: 'home#index', as: :home
+
+  # Authentication routes
+  resources :sessions
+  resources :employees
+  get 'employees/new', to: 'employees#new', as: :signup
+  get 'employee/edit', to: 'employees#edit', as: :edit_current_employee
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
+
+  # Resource routes (maps HTTP verbs to controller actions automatically):
   # Routes for regular HTML views go here...
-  
-
-
-
-
-
-
-
+  resources :stores, except: [:destroy]
 
   # You can have the root of your site routed with 'root'
-  
+  root 'home#index'
   
 end
