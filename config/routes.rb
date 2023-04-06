44e9234430c8cd0937a0a7d62df74b2089f264e1
Tianze Shou <shoutianze@outlook.com>
2023-04-06 16:41:47 -0400

    finish home controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   # Semi-static page routes
   get 'home', to: 'home#index', as: :home
+  get 'home/about', to: 'home#about', as: :about
+  get 'home/contact', to: 'home#contact', as: :contact
+  get 'home/privacy', to: 'home#privacy', as: :privacy
+  get 'home/search', to: 'home#search', as: :search
 
   # Authentication routes
   resources :sessions
