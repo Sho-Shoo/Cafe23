5013ce8bc5d658bb6be4c0ef9c779da886a8d115
Tianze Shou <shoutianze@outlook.com>
2023-04-18 00:51:08 -0400

    add tests for additional controller functionalities

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -7,6 +7,12 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should be redirected to from index to time_clock_path of logged in as regular employee" do
+    login_employee
+    get home_path
+    assert_redirected_to time_clock_path
+  end
+
   test "should get about" do
     get about_path
     assert_response :success
